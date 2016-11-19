7c960b7c05d10df94a050f704e6442233216d0d4
Anthony Li <anthli1895@gmail.com>
2016-11-19 15:44:00 -0500

    Changed port assignment

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const express = require('express');
 const app = express();
 
 // Default port to 3000 if the environment doesn't have one
-app.set('port', process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   console.log('Hello!');
 });
 
-app.listen(app.get('port'), () => {
-  console.log('Listening on port', app.get('port'));
+// Start up the server
+app.listen(port, () => {
+  console.log('Listening on port', port);
 });
