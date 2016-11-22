d518fc4194a29fa90011756f7016eddc7185428f
Anthony Li <anthli1895@gmail.com>
2016-11-22 00:22:09 -0500

    Enabled CORS

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const app = express();
 // Default port to 3000 if the environment doesn't have one
 const port = process.env.PORT || 3000;
 
+// Enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.get('/', (req, res) => {
   publicIp.v4().then(ip => {
     res.send({
