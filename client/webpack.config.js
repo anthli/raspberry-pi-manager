0d74fe2f593f6a214d349b056f37710415ccd5f7
Anthony Li <anthli1895@gmail.com>
2018-09-06 22:38:33 -0400

    Update

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(jsx?|tsx?)$/,
+        test: /\.tsx?$/,
         exclude: /node_modules/,
         loader: 'awesome-typescript-loader'
       },
